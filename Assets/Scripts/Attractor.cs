7f27d19bc08c1bd88601355fff598b9804995407
nikakogho <nikakoghuashvili@gmail.com>
2021-02-03 14:57:20 +0400

    initial commit

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Attractor : MonoBehaviour {
+    const float G = 667.4f;
+
+    float minDist = 0.0001f;
+
+    Rigidbody rb;
+    static List<Attractor> attractors = new List<Attractor>();
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        attractors.Add(this);
+    }
+    
+    void OnDisable()
+    {
+        attractors.Remove(this);
+    }
+
+    void FixedUpdate()
+    {
+        foreach(Attractor attractor in attractors)
+        {
+            if (attractor != this) Attract(attractor);
+        }
+    }
+
+    void Attract(Attractor objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance <= minDist) return;
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
---
+++ Assets/Scripts/CelestialSpawner.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class CelestialSpawner : MonoBehaviour
+{
+    public CelestialObject[] celestials;
+    public int amount = 100;
+
+    public float pointFactor;
+
+    Vector3 minPoint, maxPoint;
+
+    Vector3 RandomPoint
+    {
+        get
+        {
+            return new Vector3(Random.Range(minPoint.x, maxPoint.x), Random.Range(minPoint.y, maxPoint.y), Random.Range(minPoint.z, maxPoint.z));
+        }
+    }
+
+    void Awake()
+    {
+        float total = 0;
+
+        foreach (CelestialObject cel in celestials) total += cel.chance;
+
+        minPoint = Vector3.one * -pointFactor;
+        maxPoint = Vector3.one * pointFactor;
+
+        for(int i = 0; i < amount; i++)
+        {
+            float rand = Random.Range(0, total);
+            float sum = 0;
+
+            CelestialObject toPick = null;
+
+            foreach (CelestialObject cel in celestials)
+            {
+                sum += cel.chance;
+
+                if (sum >= rand)
+                {
+                    toPick = cel;
+                    break;
+                }
+            }
+
+            GameObject obj = Instantiate(toPick.prefab, RandomPoint, Quaternion.Euler(RandomPoint), transform);
+            float factor = Random.Range(0f, 1f);
+
+            obj.GetComponent<Rigidbody>().mass = (toPick.minMass + toPick.maxMass * factor) / 2;
+            obj.transform.localScale *= (toPick.minScaleFactor + toPick.maxScaleFactor * factor) / 2;
+        }
+    }
+
+    [System.Serializable]
+    public class CelestialObject
+    {
+        public GameObject prefab;
+        public float chance;
+        public float minMass, maxMass;
+        public float minScaleFactor, maxScaleFactor;
+    }
+}
+
